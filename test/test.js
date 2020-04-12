ab8f492bf10f75f4f100953886b76943aaabcfbb
Nagisa Sugishita <nagisa.sugishita@hotmail.co.jp>
2020-04-12 22:05:29 +0100

    add np.testing.assert_allclose

--- test/test.js
+++ test/test.js
@@ -52,3 +52,99 @@ QUnit.test('np.asarray', function(assert) {
     np.helper.es5mode();
     impl();
 });
+
+
+QUnit.test('np.isndarray', function(assert) {
+    let a = np.asarray([1, 2, 3, 4, 5, 6], [2, 3], 'float32');
+    assert.ok(np.isndarray(a), 'np.ndarray must return true for ndarray.');
+    assert.ok(
+        !np.isndarray([1, 2, 3]), 'np.ndarray must return false for js native Array.');
+    assert.ok(!np.isndarray([]), 'np.ndarray must return false for js native Array.');
+});
+
+
+QUnit.test('np.testing.assert_arraylike', function(assert) {
+    np.testing.assert_arraylike([1, Infinity, 3]);
+    np.testing.assert_arraylike([[1, Infinity, 3]]);
+    np.testing.assert_arraylike([[[1], [Infinity], [3]]]);
+    np.testing.assert_arraylike([[[1, 3], [Infinity, Infinity], [3, 1]]]);
+    np.testing.assert_arraylike([[[-1]], [[2]], [[0]], [[4]]]);
+    np.testing.assert_arraylike([]);
+
+    assert.throws(
+        function() {
+            np.testing.assert_arraylike([1, {}, 3]);
+        },
+        /invalid objects/,
+        "should raise an error"
+    );
+    assert.throws(
+        function() {
+            np.testing.assert_arraylike([1, [], 3]);
+        },
+        /mixed numeric and arrays/,
+        "should raise an error"
+    );
+    assert.throws(
+        function() {
+            np.testing.assert_arraylike([1, [1], 3]);
+        },
+        /mixed numeric and arrays/,
+        "should raise an error"
+    );
+});
+
+
+QUnit.test('np.testing.assert_allclose', function(assert) {
+    np.testing.assert_allclose([-1, 2, 0, 4], [-1, 2, 0, 4]);
+    np.testing.assert_allclose([[-1, 2], [0, 4]], [[-1, 2], [0, 4]]);
+    np.testing.assert_allclose(
+        [[[-1]], [[2]], [[0]], [[4]]], [[[-1]], [[2]], [[0]], [[4]]]);
+    np.testing.assert_allclose([], []);
+    np.testing.assert_allclose([-1, NaN, 0, 4], [-1, NaN, 0, 4]);
+    np.testing.assert_allclose([-1, Infinity, 0, 4], [-1, Infinity, 0, 4]);
+    np.testing.assert_allclose([-1, -Infinity, 0, 4], [-1, -Infinity, 0, 4]);
+
+    assert.throws(
+        function() {
+            np.testing.assert_allclose([-1, 2, 4], [-1, 2, 3]);
+        },
+        "should raise an error"
+    );
+    assert.throws(
+        function() {
+            np.testing.assert_allclose([-1, 2, 4], [-1, 2]);
+        },
+        "should raise an error"
+    );
+    assert.throws(
+        function() {
+            np.testing.assert_allclose([[-1, 2, 4]], [-1, 2, 3]);
+        },
+        "should raise an error"
+    );
+    assert.throws(
+        function() {
+            np.testing.assert_allclose([-1, 2, 0], [-1, 2, NaN]);
+        },
+        "should raise an error"
+    );
+    assert.throws(
+        function() {
+            np.testing.assert_allclose([-1, 2, Infinity], [-1, 2, NaN]);
+        },
+        "should raise an error"
+    );
+    assert.throws(
+        function() {
+            np.testing.assert_allclose([-1, 2, Infinity], [-1, 2, -Infinity]);
+        },
+        "should raise an error"
+    );
+    assert.throws(
+        function() {
+            np.testing.assert_allclose([-1, 2, 0], [-1, 2, -Infinity]);
+        },
+        "should raise an error"
+    );
+});
