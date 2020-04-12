b488a9d0d330e2474e47ed095c084a1253185d27
Nagisa Sugishita <nagisa.sugishita@hotmail.co.jp>
2020-04-12 19:52:33 +0100

    add mock of ndarray and np.asarray

--- test/test.js
+++ test/test.js
@@ -4,6 +4,31 @@ if (test_numpy_is_node) {
     np = require('../dist/ndarray.js');
 }
 
-QUnit.test('np.array', function(assert) {
-    assert.ok(np !== undefined, 'initial test');
+QUnit.test('np.ndarray', function(assert) {
+    if (np.helper.is_es6_supported) {
+        assert.ok(
+            np.ndarray !== undefined,
+            'np.ndarray must be available on ES6 compatible runtime'
+        );
+    }
+    np.helper.es5mode();
+    assert.ok(
+        np.ndarray === undefined,
+        'np.ndarray must be undefined on ES5 mode'
+    );
+});
+
+
+QUnit.test('np.asarray', function(assert) {
+    if (np.helper.is_es6_supported) {
+        let a = np.asarray([1, 2, 3, 4, 5, 6], [2, 3], 'float32');
+        assert.equal(a.shape.length, 2);
+        assert.equal(a.shape[0], 2);
+        assert.equal(a.shape[1], 3);
+    }
+    np.helper.es5mode();
+    let a = np.asarray([1, 2, 3, 4, 5, 6], [2, 3], 'float32');
+    assert.equal(a.shape.length, 2);
+    assert.equal(a.shape[0], 2);
+    assert.equal(a.shape[1], 3);
 });
