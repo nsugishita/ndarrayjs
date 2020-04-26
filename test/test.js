95ef19d449b4363e82a30dead2752b688086e7c9
Nagisa Sugishita <nagisa.sugishita@hotmail.co.jp>
2020-04-27 00:40:58 +0100

    add basic indexing

--- test/test.js
+++ test/test.js
@@ -168,6 +168,95 @@ QUnit.test('np.full', function(assert) {
 });
 
 
+QUnit.test('np.get', function(assert) {
+    // TODO Add more advanced test, such as slices, newaxis and elipsis.
+    var a;
+    a = np.asarray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [12], np.float);
+    a.shape = [3, 4];
+    a.stride = [4, 1];
+    assert.equal(a(0, 0), 1);
+    assert.equal(a(0, 1), 2);
+    assert.equal(a(0, 2), 3);
+    assert.equal(a(0, 3), 4);
+    assert.equal(a(1, 0), 5);
+    assert.equal(a(1, 1), 6);
+    assert.equal(a(1, 2), 7);
+    assert.equal(a(1, 3), 8);
+    assert.equal(a(2, 0), 9);
+    assert.equal(a(2, 1),10);
+    assert.equal(a(2, 2),11);
+    assert.equal(a(2, 3),12);
+
+    assert.equal(a(0)(0), 1);
+    assert.equal(a(0)(1), 2);
+    assert.equal(a(0)(2), 3);
+    assert.equal(a(0)(3), 4);
+    assert.equal(a(1)(0), 5);
+    assert.equal(a(1)(1), 6);
+    assert.equal(a(1)(2), 7);
+    assert.equal(a(1)(3), 8);
+    assert.equal(a(2)(0), 9);
+    assert.equal(a(2)(1),10);
+    assert.equal(a(2)(2),11);
+    assert.equal(a(2)(3),12);
+
+    a = np.asarray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [12], np.float);
+    a.shape = [2, 3, 2];
+    a.stride = [6, 2, 1];
+    assert.equal(a(0, 0, 0), 1);
+    assert.equal(a(0, 0, 1), 2);
+    assert.equal(a(0, 1, 0), 3);
+    assert.equal(a(0, 1, 1), 4);
+    assert.equal(a(0, 2, 0), 5);
+    assert.equal(a(0, 2, 1), 6);
+    assert.equal(a(1, 0, 0), 7);
+    assert.equal(a(1, 0, 1), 8);
+    assert.equal(a(1, 1, 0), 9);
+    assert.equal(a(1, 1, 1),10);
+    assert.equal(a(1, 2, 0),11);
+    assert.equal(a(1, 2, 1),12);
+
+    assert.equal(a(0)(0, 0), 1);
+    assert.equal(a(0)(0, 1), 2);
+    assert.equal(a(0)(1, 0), 3);
+    assert.equal(a(0)(1, 1), 4);
+    assert.equal(a(0)(2, 0), 5);
+    assert.equal(a(0)(2, 1), 6);
+    assert.equal(a(1)(0, 0), 7);
+    assert.equal(a(1)(0, 1), 8);
+    assert.equal(a(1)(1, 0), 9);
+    assert.equal(a(1)(1, 1),10);
+    assert.equal(a(1)(2, 0),11);
+    assert.equal(a(1)(2, 1),12);
+
+    assert.equal(a(0, 0)(0), 1);
+    assert.equal(a(0, 0)(1), 2);
+    assert.equal(a(0, 1)(0), 3);
+    assert.equal(a(0, 1)(1), 4);
+    assert.equal(a(0, 2)(0), 5);
+    assert.equal(a(0, 2)(1), 6);
+    assert.equal(a(1, 0)(0), 7);
+    assert.equal(a(1, 0)(1), 8);
+    assert.equal(a(1, 1)(0), 9);
+    assert.equal(a(1, 1)(1),10);
+    assert.equal(a(1, 2)(0),11);
+    assert.equal(a(1, 2)(1),12);
+
+    assert.equal(a(0)(0)(0), 1);
+    assert.equal(a(0)(0)(1), 2);
+    assert.equal(a(0)(1)(0), 3);
+    assert.equal(a(0)(1)(1), 4);
+    assert.equal(a(0)(2)(0), 5);
+    assert.equal(a(0)(2)(1), 6);
+    assert.equal(a(1)(0)(0), 7);
+    assert.equal(a(1)(0)(1), 8);
+    assert.equal(a(1)(1)(0), 9);
+    assert.equal(a(1)(1)(1),10);
+    assert.equal(a(1)(2)(0),11);
+    assert.equal(a(1)(2)(1),12);
+});
+
+
 QUnit.test('np.getitem', function(assert) {
     var a;
     a = np.asarray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [12], np.float);
@@ -212,6 +301,19 @@ QUnit.test('np.getitem', function(assert) {
 });
 
 
+QUnit.test('np.apply_advanced_indexing', function(assert) {
+    var a, b;
+    var S = np.indexing.Slice;
+    a = np.arange(24);
+    a._shape = [2, 3, 4];
+    a.stride = [12, 4, 1];
+    b =np.indexing.apply_basic_indexing(a, [1, new S(0, 3, 2), new S(3, 1, -1)]);
+    console.log(b.shape);
+    console.log(b.stride);
+    assert.ok(true);
+});
+
+
 QUnit.test('np.indexing.parse_slice', function(assert) {
     var a;
     a = np.indexing.parse_slice('3:4');
@@ -236,6 +338,222 @@ QUnit.test('np.indexing.parse_slice', function(assert) {
     assert.ok(a === undefined, "'null' should not be a slice");
     a = np.indexing.parse_slice([1, 2]);
     assert.ok(a === undefined, "'[1, 2]' should not be a slice");
+});
+
+
+QUnit.test('np.indexing.strip_commans', function(assert) {
+    var a;
+    a = ['3,-1,4:5'];
+    np.indexing.strip_commans(a);
+    np.testing.assert_allclose(a, ['3', '-1', '4:5'], {assert: assert});
+    a = [np.newaxis, '4:5', 4];
+    np.indexing.strip_commans(a);
+    np.testing.assert_allclose(a, [np.newaxis, '4:5', 4], {assert: assert});
+    a = [np.newaxis, '1,:,4'];
+    np.indexing.strip_commans(a);
+    np.testing.assert_allclose(a, [np.newaxis, '1', ':', '4'], {assert: assert});
+});
+
+
+QUnit.test('np.indexing.use_newaxis_and_elipsis', function(assert) {
+    var a;
+    a = [2, 4, '4:2'];
+    np.indexing.use_newaxis_and_elipsis(a);
+    np.testing.assert_allclose(
+        a,
+        [2, 4, '4:2'],
+        {assert: assert}
+    );
+
+    a = [undefined, ':', '...', 3, null];
+    np.indexing.use_newaxis_and_elipsis(a);
+    np.testing.assert_allclose(
+        a,
+        [np.newaxis, ':', np.Elipsis, 3, np.newaxis],
+        {assert: assert}
+    );
+
+    a = [undefined, '...'];
+    np.indexing.use_newaxis_and_elipsis(a);
+    np.testing.assert_allclose(
+        a,
+        [np.newaxis, np.Elipsis],
+        {assert: assert}
+    );
+});
+
+
+QUnit.test('np.indexing.is_string_integer', function(assert) {
+    assert.ok(np.indexing.is_string_integer('80'), "'80' is integer");
+    assert.ok(np.indexing.is_string_integer('0'), "'0' is integer");
+    assert.ok(np.indexing.is_string_integer('-1'), "'-1' is integer");
+    assert.ok(!np.indexing.is_string_integer('02'), "'02' is not integer");
+    assert.ok(!np.indexing.is_string_integer('-01'), "'-01' is not integer");
+    assert.ok(!np.indexing.is_string_integer('00'), "'00' is not integer");
+    assert.ok(!np.indexing.is_string_integer('1.0'), "'1.0' is not integer");
+    assert.ok(!np.indexing.is_string_integer('.0'), "'.0' is not integer");
+    assert.ok(!np.indexing.is_string_integer('1a'), "'1a' is not integer");
+    assert.ok(!np.indexing.is_string_integer('a'), "'a' is not integer");
+    assert.ok(!np.indexing.is_string_integer('-'), "'-' is not integer");
+});
+
+
+QUnit.test('np.indexing.parse_integer_and_slice', function(assert) {
+    var a;
+
+    a = [4, '1', '3:4', np.newaxis, 0];
+    np.indexing.parse_integer_and_slice(a);
+    np.testing.assert_allclose(
+        a, [4, 1, new np.indexing.Slice(3, 4), np.newaxis, 0], {assert: assert});
+
+    a = [0, null, ':', np.Elipsis, ':'];
+    np.indexing.parse_integer_and_slice(a);
+    np.testing.assert_allclose(
+        a,
+        [0, null, new np.indexing.Slice(), np.Elipsis, new np.indexing.Slice()],
+        {assert: assert}
+    );
+
+    assert.throws(
+        function() {
+            np.indexing.parse_integer_and_slice([0, 'null', ':']);
+        },
+        /invalid item/,
+        "should raise an error"
+    );
+
+    assert.throws(
+        function() {
+            np.indexing.parse_integer_and_slice(['1.2:']);
+        },
+        /invalid item/,
+        "should raise an error"
+    );
+
+    assert.throws(
+        function() {
+            np.indexing.parse_integer_and_slice(['1.2']);
+        },
+        /invalid item/,
+        "should raise an error"
+    );
+});
+
+
+QUnit.test('np.indexing.expand_elipsis', function(assert) {
+    var a, expected, slice;
+    slice = new np.indexing.Slice();
+
+    a = [1, 3, np.Elipsis, -1];
+    np.indexing.expand_elipsis(a, [10, 20, 30, 40]);
+    expected = [1, 3, slice, -1];
+    np.testing.assert_allclose(a, expected, {assert: assert});
+
+    a = [1, 3, np.Elipsis];
+    np.indexing.expand_elipsis(a, [10, 20, 30, 40]);
+    expected = [1, 3, slice, slice];
+    np.testing.assert_allclose(a, expected, {assert: assert});
+
+    a = [np.Elipsis, new np.indexing.Slice(1, 2)];
+    np.indexing.expand_elipsis(a, [10, 20, 30, 40]);
+    expected = [slice, slice, slice, new np.indexing.Slice(1, 2)];
+    np.testing.assert_allclose(a, expected, {assert: assert});
+
+    assert.throws(
+        function() {
+            a = [np.Elipsis, new np.indexing.Slice(1, 2), np.Elipsis];
+            np.indexing.expand_elipsis(a, [10, 20, 30, 40]);
+        },
+        /more than one Elipsis found/,
+        "should raise an error"
+    );
+});
+
+
+QUnit.test('np.indexing.normalize_index', function(assert) {
+    var a, expected, slice;
+    slice = new np.indexing.Slice();
+
+    a = [1, 2, -2, 0];
+    np.indexing.normalize_index(a, [10, 20, 30, 40]);
+    expected = [1, 2, 28, 0];
+    np.testing.assert_allclose(a, expected, {assert: assert});
+
+    a = [-5, np.newaxis, -1, np.newaxis];
+    np.indexing.normalize_index(a, [10, 20]);
+    expected = [5, np.newaxis, 19, np.newaxis];
+    np.testing.assert_allclose(a, expected, {assert: assert});
+
+    a = [-5];
+    np.indexing.normalize_index(a, [20]);
+    expected = [15];
+    np.testing.assert_allclose(a, expected, {assert: assert});
+
+    assert.throws(
+        function() {
+            a = [-10, 3];
+            np.indexing.normalize_index(a, [5, 5]);
+        },
+        /out of range/,
+        "should raise an error"
+    );
+
+    assert.throws(
+        function() {
+            a = [10, 3];
+            np.indexing.normalize_index(a, [5, 5]);
+        },
+        /out of range/,
+        "should raise an error"
+    );
+
+    assert.throws(
+        function() {
+            a = [0, new np.indexing.Slice(10, 1)];
+            np.indexing.normalize_index(a, [5, 5]);
+        },
+        /out of range/,
+        "should raise an error"
+    );
+
+    assert.throws(
+        function() {
+            a = [0, new np.indexing.Slice(-10, 1)];
+            np.indexing.normalize_index(a, [5, 5]);
+        },
+        /out of range/,
+        "should raise an error"
+    );
+});
+
+
+QUnit.test('np.indexing.add_tail', function(assert) {
+    var a, expected, slice;
+    slice = new np.indexing.Slice();
+
+    a = [1, 3, -1];
+    np.indexing.add_tail(a, [10, 20, 30, 40]);
+    expected = [1, 3, -1, slice];
+    np.testing.assert_allclose(a, expected, {assert: assert});
+
+    a = [-1];
+    np.indexing.add_tail(a, [10, 20, 30, 40]);
+    expected = [-1, slice, slice, slice];
+    np.testing.assert_allclose(a, expected, {assert: assert});
+
+    a = [new np.indexing.Slice(1)];
+    np.indexing.add_tail(a, [10, 20, 30, 40]);
+    expected = [a[0], slice, slice, slice];
+    np.testing.assert_allclose(a, expected, {assert: assert});
+
+    assert.throws(
+        function() {
+            a = [1, 2, new np.indexing.Slice(1), 3, 4];
+            np.indexing.add_tail(a, [10, 20, 30, 40]);
+        },
+        /too many/,
+        "should raise an error"
+    );
 });
 
 
